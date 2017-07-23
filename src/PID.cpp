3ba0f34c89be5b5a10351f0f58cef0293a81e941
solix <soheil.jahanshahi@gmail.com>
2017-07-23 20:31:29 +0200

    car drives around but oscilates

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,33 @@ PID::PID() {}
 
 PID::~PID() {}
 
+
+
 void PID::Init(double Kp, double Ki, double Kd) {
+
+	//coefficient
+	this -> Kp = Kp;
+	this -> Ki =  Ki;
+	this -> Kd = Kd;
+	this -> p_error=0;
+    this -> i_error=0;
+    this -> d_error=0;
+    
+
 }
 
 void PID::UpdateError(double cte) {
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error+=cte;
+
+	// cout<<"PID Error"<<p_error<<i_error<<d_error<<endl;
 }
 
 double PID::TotalError() {
+
+	return Kp*p_error+Ki*i_error+Kd*d_error;
 }
+
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,17 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  // Initialize the pid variable.
+  double kp0,ki0,kd0 ;
+  kp0 = atof(argv[1]);
+  ki0 = atof(argv[2]);
+  kd0 = atof(argv[3]);
+  pid.Init(kp0,ki0,kd0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +62,12 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
+
+           pid.UpdateError(cte);         
+           steer_value = pid.TotalError();
+          
+
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
